ed85a5d4e886cb9abba5c4821f3a6adca1e814b3
thiago-you <thiago.youx@gmail.com>
2024-05-26 02:06:59 -0300

    implementing login default

--- src/main/kotlin/br/pucpr/carsrent/users/requests/LoginRequest.kt
+++ src/main/kotlin/br/pucpr/carsrent/users/requests/LoginRequest.kt
@@ -1,6 +1,10 @@
 package br.pucpr.carsrent.users.requests
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class LoginRequest(
+    @Schema(example = "admin@admin.com")
     var email: String? = null,
+    @Schema(example = "admin")
     var password: String? = null
 )
